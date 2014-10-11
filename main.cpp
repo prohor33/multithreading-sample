11fe70bcf8c89ea4cbb7e28ea0b0a337bd81f59d
Gladkikh Prohor <prohor33@gmail.com>
2014-10-11 21:51:24 +0400

    it’s working now!

--- main.cpp
+++ main.cpp
@@ -25,7 +25,7 @@ public:
   std::shared_ptr<std::thread> operator->() {
     return thread_;
   }
-  
+
 private:
   std::shared_ptr<std::thread> thread_;
 };
@@ -36,10 +36,10 @@ struct Message {
     TO_FINISH = 1,
     FINISHED = 2
   };
-  
+
   Message(MsgType tmp_type, INT tmp_data = 0) :
   type(tmp_type), data(tmp_data) {};
-  
+
   MsgType type;
   INT data;
 };
@@ -49,7 +49,7 @@ public:
   std::queue<std::shared_ptr<Message>>& msg_queue() { return msg_queue_; };
   std::mutex& mutex() { return mutex_; };
   std::condition_variable& cond() { return cond_; };
-  
+
 private:
   std::queue<std::shared_ptr<Message>> msg_queue_;
   std::mutex mutex_;
@@ -66,19 +66,19 @@ public:
     }
     return instance_.get();
   }
-  
+
   MsgQueue* msgs_to_workers() {
     return msgs_to_workers_.get();
   }
   MsgQueue* msgs_to_main() {
     return msgs_to_main_.get();
   }
-  
+
 private:
   Telegraph() :
   msgs_to_workers_(new MsgQueue()),
   msgs_to_main_(new MsgQueue()) {};
-  
+
   static std::shared_ptr<Telegraph> instance_;
   static std::mutex mutex_;
   std::shared_ptr<MsgQueue> msgs_to_workers_;
@@ -92,29 +92,30 @@ class PrimeCollector {
 public:
   void Run() {
     while (true) {
-      std::unique_lock<std::mutex> lk(Telegraph::Instance()->msgs_to_workers()->mutex());
+      std::unique_lock<std::mutex> lk(
+          Telegraph::Instance()->msgs_to_workers()->mutex());
 
       if (!Telegraph::Instance()->msgs_to_workers()->msg_queue().empty()) {
-        std::shared_ptr<Message> pMsg = Telegraph::Instance()->msgs_to_workers()->msg_queue().front();
-        
+        std::shared_ptr<Message> pMsg =
+            Telegraph::Instance()->msgs_to_workers()->msg_queue().front();
+
         if (pMsg->type == Message::DATA) {
           Factorize(pMsg->data);
         } else if (pMsg->type == Message::TO_FINISH) {
           NotifyMain(Message::FINISHED);
           return;  // do not pop the message
         }
-        
+
         Telegraph::Instance()->msgs_to_workers()->msg_queue().pop();
       } else {
         Telegraph::Instance()->msgs_to_workers()->cond().wait(lk);
       }
     }
   }
-  
+
   void Factorize(INT number) {
-    std::cout << "nubmer: " << number << std::endl;
     primes_pow_.clear();
-    
+
     INT z = 2;
     while (z * z <= number) {
       if (number % z == 0) {
@@ -124,30 +125,20 @@ public:
         z++;
       }
     }
-    
+
     if (number > 1)
       AddPrime(number);
-    
-    PrintTempResult();
+
     MergeMaxPows(primes_pow_);
-    PrintResult();
   }
-  
-  void PrintTempResult() const {
-    std::cout << "temp result: ";
-    for (auto prime : primes_pow_) {
-      std::cout << prime.first << "^" << prime.second << ", ";
-    }
-    std::cout << std::endl;
-  }
+
   void PrintResult() const {
-        std::cout << "result: ";
     for (auto prime : primes_max_pow_) {
-      std::cout << prime.first << "^" << prime.second << ", ";
+      std::cout << prime.first << "^" << prime.second << " * ";
     }
     std::cout << std::endl;
   }
-  
+
   void MergeMaxPows(const std::map<INT, INT>& input_primes_pow) {
     for (auto prime : input_primes_pow) {
       if (primes_max_pow_.find(prime.first) == primes_max_pow_.end()) {
@@ -158,23 +149,24 @@ public:
         primes_max_pow_[prime.first] = prime.second;
     }
   }
-  
+
   const std::map<INT, INT>& primes_max_pow() const {
     return primes_max_pow_;
   }
-  
+
 private:
   void AddPrime(INT prime) {
     if (primes_pow_.find(prime) == primes_pow_.end())
       primes_pow_[prime] = 0;
     primes_pow_[prime]++;
   }
-  
+
   void NotifyMain(Message::MsgType msg_type) {
     {
-      std::unique_lock<std::mutex> lk(Telegraph::Instance()->msgs_to_main()->mutex());
-//      std::cout << "push notify msg" << std::endl;
-      Telegraph::Instance()->msgs_to_main()->msg_queue().push(std::shared_ptr<Message>(new Message(msg_type)));
+      std::unique_lock<std::mutex> lk(
+          Telegraph::Instance()->msgs_to_main()->mutex());
+      Telegraph::Instance()->msgs_to_main()->msg_queue().push(
+          std::shared_ptr<Message>(new Message(msg_type)));
     }
     Telegraph::Instance()->msgs_to_main()->cond().notify_one();
   }
@@ -184,57 +176,66 @@ private:
 };
 
 int main(int argc, const char * argv[]) {
-  
-  int threads_number = 4;  // TODO: ask user?
-  
-  std::cout << "please enter your numbers: " << std::endl;
-  
+
+  int threads_number = 100;  // TODO: ask user?
+
+  std::cout << "please, enter your numbers: " << std::endl;
+
   std::list<std::shared_ptr<ThreadWrapper>> threads_list;
   std::list<std::shared_ptr<PrimeCollector>> primes_collectors_list;
-  
+
   for (int i = 0; i < threads_number; ++i) {
     std::shared_ptr<PrimeCollector> prime_collector =
     std::shared_ptr<PrimeCollector>(new PrimeCollector());
     std::shared_ptr<ThreadWrapper> pThread =
-    std::shared_ptr<ThreadWrapper>(new ThreadWrapper(std::shared_ptr<std::thread>(new std::thread(&PrimeCollector::Run, prime_collector))));
+    std::shared_ptr<ThreadWrapper>(new ThreadWrapper(
+            std::shared_ptr<std::thread>(new std::thread(&PrimeCollector::Run,
+                                                         prime_collector))));
     primes_collectors_list.push_back(prime_collector);
     threads_list.push_back(pThread);
   }
-  
+
   while (!std::cin.eof()) {
     int64_t tmp;
     std::cin >> tmp;
     if (tmp < 1) {
-      std::cout << "bad input: " << tmp << " < 1" << std::endl;
+      std::cout << "bad input: input number = " << tmp <<
+          " is below zero" << std::endl;
       break;
     }
     INT number = static_cast<INT>(tmp);
-    
+
     {
-      std::unique_lock<std::mutex> lk(Telegraph::Instance()->msgs_to_workers()->mutex());
-      std::cout << "push data msg" << std::endl;
-      Telegraph::Instance()->msgs_to_workers()->msg_queue().push(std::shared_ptr<Message>(new Message(Message::DATA, number)));
+      std::unique_lock<std::mutex> lk(
+          Telegraph::Instance()->msgs_to_workers()->mutex());
+      // push data message
+      Telegraph::Instance()->msgs_to_workers()->msg_queue().push(
+          std::shared_ptr<Message>(new Message(Message::DATA, number)));
     }
     Telegraph::Instance()->msgs_to_workers()->cond().notify_one();
   }
-  
+
   {
-    std::unique_lock<std::mutex> lk(Telegraph::Instance()->msgs_to_workers()->mutex());
-    std::cout << "push comand to terminate" << std::endl;
-    Telegraph::Instance()->msgs_to_workers()->msg_queue().push(std::shared_ptr<Message>(new Message(Message::TO_FINISH)));
+    std::unique_lock<std::mutex> lk(
+        Telegraph::Instance()->msgs_to_workers()->mutex());
+    //  push comand to terminate
+    Telegraph::Instance()->msgs_to_workers()->msg_queue().push(
+        std::shared_ptr<Message>(new Message(Message::TO_FINISH)));
   }
-      Telegraph::Instance()->msgs_to_workers()->cond().notify_all();
-  
+  Telegraph::Instance()->msgs_to_workers()->cond().notify_all();
+
   int threads_terminated = 0;
   while (true) {
-    std::unique_lock<std::mutex> lk(Telegraph::Instance()->msgs_to_main()->mutex());
-    
+    std::unique_lock<std::mutex> lk(
+        Telegraph::Instance()->msgs_to_main()->mutex());
+
     if (!Telegraph::Instance()->msgs_to_main()->msg_queue().empty()) {
-      std::shared_ptr<Message> pMsg = Telegraph::Instance()->msgs_to_main()->msg_queue().front();
+      std::shared_ptr<Message> pMsg =
+          Telegraph::Instance()->msgs_to_main()->msg_queue().front();
       if (pMsg->type == Message::FINISHED) {
         threads_terminated++;
         if (threads_terminated == threads_number) {
-          std::cout << "all threads have completed their work" << std::endl;
+          // all threads have completed their work
           break;
         }
       }
@@ -244,8 +245,7 @@ int main(int argc, const char * argv[]) {
     }
   }
 
-  std::cout << "merge results from different threads..." << std::endl;
-
+  //  merge results from different threads
   auto it_first = primes_collectors_list.begin();
   if (it_first == primes_collectors_list.end()) {
     std::cout << "error: no prime collectors" << std::endl;
@@ -258,8 +258,6 @@ int main(int argc, const char * argv[]) {
     ++it;
   }
   (*it_first)->PrintResult();
-
-  std::cout << "ending..." << std::endl;
 
   return 0;
 }
